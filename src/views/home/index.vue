6d93277ca3edf1155228cbe68fa6668ad4253867
zhongjunwei <liwenxin@wisight.cn>
2022-06-29 10:49:52 +0800

    update

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <van-button type="primary">主要按钮</van-button>
   <nut-navbar :left-show="false" :title="$t('tabbar.home')" />
+  <van-button type="primary">主要按钮</van-button>
   <header class="header"> <img src="https://www.sunniejs.cn/static/weapp/logo.png" /><span> VUE H5开发模板</span> </header>
   <p class="intro-header">
     {{ $t('introduction') }}
@@ -29,54 +29,54 @@
 </template>
 
 <script lang="ts" setup>
-  import { computed } from 'vue';
-  import { useUserStore } from '/@/store/modules/user';
-  import { setLang } from '/@/i18n';
+import { computed } from 'vue';
+import { useUserStore } from '/@/store/modules/user';
+import { setLang } from '/@/i18n';
 
-  const userStore = useUserStore();
-  const getUserInfo = computed(() => {
-    const { name = '' } = userStore.getUserInfo || {};
-    return name;
-  });
-  const changeLang = (type) => {
-    setLang(type);
-  };
+const userStore = useUserStore();
+const getUserInfo = computed(() => {
+  const { name = '' } = userStore.getUserInfo || {};
+  return name;
+});
+const changeLang = (type) => {
+  setLang(type);
+};
 </script>
 <style lang="scss">
-  .header {
-    display: flex;
-    justify-content: center;
-    margin: 26px 0 10px;
-    padding: 0 20px;
-    height: 50px;
-    height: 30px;
-    font-size: 20px;
-  }
+.header {
+  display: flex;
+  justify-content: center;
+  margin: 26px 0 10px;
+  padding: 0 20px;
+  height: 50px;
+  height: 30px;
+  font-size: 20px;
+}
 
-  .intro-title {
-    text-align: center;
-  }
+.intro-title {
+  text-align: center;
+}
 
-  .intro-header {
-    height: 30px;
-    font-size: 16px;
-    text-align: center;
-  }
+.intro-header {
+  height: 30px;
+  font-size: 16px;
+  text-align: center;
+}
 
-  .supportList {
-    margin: 0 16px;
+.supportList {
+  margin: 0 16px;
 
-    .nut-icon {
-      color: green;
-    }
+  .nut-icon {
+    color: green;
   }
+}
 
-  .github-icon {
-    margin-top: 4px;
-    font-size: 24px;
-  }
+.github-icon {
+  margin-top: 4px;
+  font-size: 24px;
+}
 
-  .btn-wrap {
-    margin: 20px;
-  }
+.btn-wrap {
+  margin: 20px;
+}
 </style>
