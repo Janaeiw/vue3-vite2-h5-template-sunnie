8fa2b964bc3d78765bb24cc19e00bac3a3c104c1
zhongjunwei <liwenxin@wisight.cn>
2022-07-05 16:49:54 +0800

    all uodate config

--- src/views/member/index.vue
+++ src/views/member/index.vue
@@ -1,7 +1,7 @@
 <template>
   <nut-navbar :left-show="false" :title="$t('tabbar.member')" />
   <div class="avatar-wrap">
-    <nut-avatar class="avatar" size="large" icon="../../../public/touxiang.jpg" />
+    <nut-avatar class="avatar" size="large" icon="/public/images/avatar.jpg" />
     <div class="member-detail">
       <p class="nickname"
         >Janaeiw
