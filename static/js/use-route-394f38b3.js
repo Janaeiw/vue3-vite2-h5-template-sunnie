7629944e0e5270ec06ae3538958cae4b4064c167
Janaeiw <40946510+Janaeiw@users.noreply.github.com>
2025-09-28 09:56:38 +0000

    Deploying to gh-pages from @ Janaeiw/vue3-vite2-h5-template-sunnie@bb2d6ac1ba411ce5b0c3042dce96563e2ae7736e 🚀

---
+++ static/js/use-route-394f38b3.js
@@ -0,0 +1,1 @@
+import{a4 as r}from"./index-6573ff2a.js";const o={to:[String,Object],url:String,replace:Boolean};function e(){const o=r().proxy;return()=>function({to:r,url:o,replace:e,$router:t}){r&&t?t[e?"replace":"push"](r):o&&(e?location.replace(o):location.href=o)}(o)}export{o as r,e as u};
